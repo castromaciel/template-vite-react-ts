d0abbf120710e8d59b4db10104ffa0fc6d7ccd56
castromaciel <castromaciel00@gmail.com>
2023-03-31 21:46:40 -0300

    feat: add vitest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,13 @@
 export default {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFiles: ['./jest.setup.ts']
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test|tests).[tj]s?(x)'
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  setupFiles: ['./jest.setup.ts'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
 }
